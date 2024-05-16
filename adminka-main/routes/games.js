1365bba788577c67a94b7237499b78475bda80bd
kingplay22890 <149995363+kingplay22890@users.noreply.github.com>
2024-05-16 21:55:15 +0600

    Add files via upload

---
+++ adminka-main/routes/games.js
@@ -0,0 +1,23 @@
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
+const { getAllGames, findGameById, deleteGame, updateGamesFile, checkIsTitleInArray, updateGamesArray } = require("../middlewares/games");
+
+const gamesRouter = require("express").Router();
+
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.delete(
+  "/games/:id", 
+getAllGames,
+findGameById,
+deleteGame,
+updateGamesFile,
+sendUpdatedGames);
+
+gamesRouter.post(
+  "/games", 
+getAllGames,
+checkIsTitleInArray,
+updateGamesArray,
+updateGamesFile,
+sendUpdatedGames);
+
+module.exports = gamesRouter;
